c437121edea2e0bf35b1952db05cbe2549db7518
faliyaaakash <aakashfaliya1@gmail.com>
2026-01-24 18:07:59 +0530

    commit to passwd_generator

--- passwd_generator/src/main.jsx
+++ passwd_generator/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Login from './loginpage.jsx'
 import "./tailwind.css"
 
 createRoot(document.getElementById('root')).render(
