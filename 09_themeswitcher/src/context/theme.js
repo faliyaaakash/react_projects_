1f389989dd99567f3c038d775e2ec9a35810ece7
faliyaaakash <aakashfaliya1@gmail.com>
2026-01-24 22:25:45 +0530

    initail commit to themeswitcher

---
+++ 09_themeswitcher/src/context/theme.js
@@ -0,0 +1,14 @@
+import { createContext,useContext } from "react";
+
+export const themecontext=createContext({
+    themeMode:"light",
+    darkTheme:()=>{},
+    lightTheme:()=>{},
+})
+
+export const ThemeProvider=themecontext.Provider()
+
+
+export default function useTheme(){
+   return useContext(themecontext);
+}
